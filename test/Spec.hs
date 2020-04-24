4209cb3b987bbdbb9115d6925a1f3944712d007f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-24 20:33:06 +1000

    Added a test framework

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = describe "Math" $ do
+    it "basic" $
+        1 `shouldBe` 1
