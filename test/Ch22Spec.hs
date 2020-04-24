7ec4f0ec9b253e71c9bdcfefa8fd1330fc24d6a1
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-24 22:47:17 +1000

    Chapter 22 simple exercise

--- test/Ch22Spec.hs
+++ test/Ch22Spec.hs
@@ -1,9 +1,31 @@
 module Ch22Spec (spec) where
 
+import Data.Char
 import Test.Hspec
 
 spec :: Spec
 spec =
-    describe "Foo" $ do
-        it "bar" $
-            2 `shouldBe` 2
+    describe "Compose" $ do
+        it "composed" $
+            composed "Julie" `shouldBe` "EILUJ"
+
+        it "fmapped" $
+            fmapped "Julie" `shouldBe` "EILUJ"
+
+        it "tupled" $
+            tupled "Julie" `shouldBe` ("eiluJ", "JULIE")
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap rev cap
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> rev <*> cap
